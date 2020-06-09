edb9a7dc50d838d1dd4bce6dd522e250b88cea32
Yaming.shi <404304809@qq.com>
2020-06-09 11:51:19 +0800

    相应拦截器统一错误处理

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import qs from 'qs'
 
-// 添加请求拦截器, 让post请求的请求体格式为urlencoded格式
+// 添加请求拦截器, 请求发送之前
+// 让post请求的请求体格式为urlencoded格式
 axios.interceptors.request.use(function (config) {
 	const { method, data } = config
 
@@ -15,12 +16,17 @@ axios.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 // 在请求返回之后, 且在我们制定的请求响应回调函数之前
+// 1. 成功回调, 返回response.data
+// 2.失败回调, 错误统一处理
 axios.interceptors.response.use(
 	function (response) {
 		return response.data
 	},
 	function (error) {
-		return Promise.reject(error)
+		// 返回一个pending状态的promise, 中断promise链
+		return new Promise(() => {})
+
+		// return Promise.reject(error)
 	}
 )
 export default axios
