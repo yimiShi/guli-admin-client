b6f11864da5370edc3639dfd76d2a3c565b27bd9
Yaming.shi <404304809@qq.com>
2020-06-10 11:00:33 +0800

    jsonp获取天气, 自定义button组件

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,14 @@
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import { message } from 'antd'
 
 const BASE = 'http://localhost:3000'
 
+/**
+ * 登陆
+ * @param {} username
+ * @param {*} password
+ */
 export const reqLogin = (username, password) =>
 	/* ajax({
 		method: 'post',
@@ -37,3 +44,20 @@ export const reqLogin = (username, password) =>
 // 		console.log('error', err)
 // 	}
 // )
+
+/**
+ * 发送jsonp请求得到天气信息
+ */
+export const reqWeather = (city) => {
+	return new Promise((resolve, reject) => {
+		const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+		jsonp(url, {}, (error, data) => {
+			if (!error && data.error === 0) {
+				const { weather, dayPictureUrl } = data.results[0].weather_data[0]
+				resolve({ dayPictureUrl, weather })
+			} else {
+				message.error('获取天气失败')
+			}
+		})
+	})
+}
